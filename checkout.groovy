b17ae7760b0668e2994116e54c2e2fde3fc68c68
Ngwe Htun <ngwe.h@rvconnex.com>
2025-02-03 15:31:21 +0700

    feat: Init repo

---
+++ checkout.groovy
@@ -0,0 +1,42 @@
+def checkoutToJenkinsConfRepo() {
+    def branch = 'develop'
+    def repoUrl = 'https://github.com/ngwehtun-rv/jenkins-conf.git'
+    def credentialsId = 'hiki-github-cred'
+
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: "*/${branch}"]],
+        userRemoteConfigs: [[url: repoUrl, credentialsId: credentialsId]]
+    ])
+}
+
+def checkoutToXDRRepo(branch, path) {
+    def repoUrl = 'https://github.com/ngwehtun-rv/xdr-research.git'
+    def credentialsId = 'hiki-github-cred'
+
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: "*/${branch}"]],
+        doGenerateSubmoduleConfigurations: false,
+        extensions: [
+                [$class: 'CloneOption', depth: 1, noTags: true, reference: '', shallow: true, timeout: 30],
+                [$class: 'SparseCheckoutPaths', sparseCheckoutPaths: [[path: path]]]
+        ],
+        submoduleCfg: [],
+        userRemoteConfigs: [[
+            url: repoUrl,
+            credentialsId: 'hiki-github-cred'
+        ]]
+    ])
+
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: "*/${branch}"]],
+        userRemoteConfigs: [[url: repoUrl, credentialsId: credentialsId]]
+    ])
+}
+
+return [
+    xdrRepo: checkoutToXDRRepo,
+    confRepo: checkoutToJenkinsConfRepo
+]
